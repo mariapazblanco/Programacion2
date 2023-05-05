1decd2e622d62c6704e0cc471d93a24c0bba7265
mariapazblanco <pazblanco01@gmail.com>
2023-05-05 17:04:03 -0300

    ejercicios resueltos

---
+++ EJERCICIOS/Practica 2/Ejercicio6.py
@@ -0,0 +1,49 @@
+"""Type, Comprensión de Listas, Sorted y Filter."""
+
+from typing import List, Union
+
+
+def numeros_al_final_basico(lista: List[Union[float, str]]) -> List[Union[float, str]]:  # noqa: E501
+    """Toma una lista de enteros y strings y devuelve una lista con todos los
+    elementos numéricos al final.
+
+    Restricciones:
+        - Utilizar un bucle FOR.
+        - Utilizar la función type.
+        - No utilizar índices.
+    """
+    numeros = []
+    no_numeros = []
+    for elementos in lista:
+        if type(elementos) == int:
+            numeros.append(elementos)
+        else:
+            no_numeros.append(elementos)
+    return no_numeros + numeros
+
+
+# NO MODIFICAR - INICIO
+assert numeros_al_final_basico([3, "a", 1, "b", 10, "j"]) == ["a", "b", "j", 3, 1, 10]  # noqa: E501
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def numeros_al_final_comprension(lista: List[Union[float, str]]) -> List[Union[float, str]]:  # noqa: E501
+    """Re-escribir utilizando comprensión de listas.
+
+    Restricciones:
+        - No utilizar bucles.
+        - Utilizar dos comprensiones de listas.
+    """
+
+    numeros = [elementos for elementos in lista if type(elementos == int)]
+    no_numeros = [elementos for elementos in lista if type(elementos) != int ]
+
+    return no_numeros + numeros
+
+
+# NO MODIFICAR - INICIO
+assert numeros_al_final_comprension([3, "a", 1, "b", 10, "j"]) == ["a", "b", "j", 3, 1, 10]  # noqa: E501
+# NO MODIFICAR - FIN
